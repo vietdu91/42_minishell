31ef6dd0c5100992c41e814d24aab3fc1c7049d7
Dimitri YOULA <dyoula@e2r4p17.clusters.42paris.fr>
2022-02-09 00:49:00 +0100

    cmt

--- objs/main.d
+++
@@ -1,4 +0,0 @@
-objs/main.o: srcs/main.c srcs/../includes/minishell.h \
-  includes/./parsing.h includes/./bultin.h includes/./checkers.h \
-  includes/./fun.h includes/./sig.h includes/./utils.h \
-  includes/./errors.h includes/./memory.h
