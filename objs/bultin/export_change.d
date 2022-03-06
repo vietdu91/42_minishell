65f943148dc8d4aedb9964b9c576faa5b2b79470
Emmanuel TRAN <emtran@e3r5p14.clusters.42paris.fr>
2022-03-06 16:03:49 +0100

    export presque parfait - reparsing a faire

--- objs/bultin/export_change.d
+++
@@ -1,6 +0,0 @@
-objs/bultin/export_change.o: srcs/bultin/export_change.c \
-  srcs/bultin/../../includes/minishell.h includes/./parsing.h \
-  includes/./bultin.h includes/./checkers.h includes/./fun.h \
-  includes/./sig.h includes/./utils.h includes/./errors.h \
-  includes/./memory.h includes/./lexer.h includes/./exec.h \
-  includes/./minishell.h
--- objs/parsing/crypting_parser_quotes.d
+++
@@ -1,7 +0,0 @@
-objs/parsing/crypting_parser_quotes.o: \
-  srcs/parsing/crypting_parser_quotes.c \
-  srcs/parsing/../../includes/minishell.h includes/./parsing.h \
-  includes/./bultin.h includes/./checkers.h includes/./fun.h \
-  includes/./sig.h includes/./utils.h includes/./errors.h \
-  includes/./memory.h includes/./lexer.h includes/./exec.h \
-  includes/./minishell.h
