e926b285f02c3e8410a0a899acfaab97bdcee324
Manu <tran.emmanuel@outlook.fr>
2022-03-15 14:55:29 +0100

    prout

--- objs/memory/free_lists.d
+++
@@ -1,7 +0,0 @@
-objs/memory/free_lists.o: srcs/memory/free_lists.c \
- includes/../includes/minishell.h includes/../includes/./parsing.h \
- includes/../includes/./bultin.h includes/../includes/./checkers.h \
- includes/../includes/./fun.h includes/../includes/./sig.h \
- includes/../includes/./utils.h includes/../includes/./errors.h \
- includes/../includes/./memory.h includes/../includes/./lexer.h \
- includes/../includes/./exec.h
