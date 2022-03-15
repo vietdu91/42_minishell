aa9d92b8a40ae66e86bd2ca06ba31ab3b28b745a
Manu <tran.emmanuel@outlook.fr>
2022-03-15 14:17:28 +0100

    fin cd

--- objs/exec/exec_maestro.d
+++
@@ -1,7 +0,0 @@
-objs/exec/exec_maestro.o: srcs/exec/exec_maestro.c \
- includes/../includes/minishell.h includes/../includes/./parsing.h \
- includes/../includes/./bultin.h includes/../includes/./checkers.h \
- includes/../includes/./fun.h includes/../includes/./sig.h \
- includes/../includes/./utils.h includes/../includes/./errors.h \
- includes/../includes/./memory.h includes/../includes/./lexer.h \
- includes/../includes/./exec.h includes/../includes/././minishell.h
--- objs/parsing/line_to_linked_list.d
+++
@@ -1,7 +0,0 @@
-objs/parsing/line_to_linked_list.o: srcs/parsing/line_to_linked_list.c \
- includes/../includes/minishell.h includes/../includes/./parsing.h \
- includes/../includes/./bultin.h includes/../includes/./checkers.h \
- includes/../includes/./fun.h includes/../includes/./sig.h \
- includes/../includes/./utils.h includes/../includes/./errors.h \
- includes/../includes/./memory.h includes/../includes/./lexer.h \
- includes/../includes/./exec.h includes/../includes/././minishell.h
