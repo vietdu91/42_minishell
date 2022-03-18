3b68bf5184ecbb8320e629fe33d21008a76071d9
Manu <tran.emmanuel@outlook.fr>
2022-03-18 18:18:31 +0100

    il n'exit plus le proc

--- objs/parsing/find_in_env.d
+++
@@ -1,7 +0,0 @@
-objs/parsing/find_in_env.o: srcs/parsing/find_in_env.c \
- includes/../includes/minishell.h includes/../includes/./parsing.h \
- includes/../includes/./bultin.h includes/../includes/./checkers.h \
- includes/../includes/./fun.h includes/../includes/./sig.h \
- includes/../includes/./utils.h includes/../includes/./errors.h \
- includes/../includes/./memory.h includes/../includes/./lexer.h \
- includes/../includes/./exec.h
