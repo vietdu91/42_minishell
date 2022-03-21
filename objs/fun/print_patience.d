ce792151ee1e3db6756f1f6ab8fcf53014ca6332
QnYosa <dimitriyoula@gmail.com>
2022-03-22 00:44:26 +0100

    debug path

--- objs/fun/print_patience.d
+++
@@ -1,7 +0,0 @@
-objs/fun/print_patience.o: srcs/fun/print_patience.c \
- srcs/fun/../../includes/minishell.h srcs/fun/../../includes/./parsing.h \
- srcs/fun/../../includes/./bultin.h srcs/fun/../../includes/./checkers.h \
- srcs/fun/../../includes/./fun.h srcs/fun/../../includes/./sig.h \
- srcs/fun/../../includes/./utils.h srcs/fun/../../includes/./errors.h \
- srcs/fun/../../includes/./memory.h srcs/fun/../../includes/./lexer.h \
- srcs/fun/../../includes/./exec.h
