3c22b4a8609138b06f1707c6ab91be75db8cab3b
Manu <tran.emmanuel@outlook.fr>
2022-04-08 20:11:07 +0200

    expand hrdc en cours

--- srcs/exec/expand_heredoc.c
+++ srcs/exec/expand_heredoc.c
@@ -6,13 +6,51 @@
 /*   By: dyoula <dyoula@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/08 18:27:18 by dyoula            #+#    #+#             */
-/*   Updated: 2022/04/08 20:04:35 by dyoula           ###   ########.fr       */
+/*   Updated: 2022/04/08 20:10:36 by dyoula           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+
 #include "../../includes/minishell.h"
 
-int	modify_heredoc(char *hdoc, char *limitator)
+int	quotes_or_not(char *str)
+{
+	int	i;
+
+	i = -1;
+	while (str[++i])
+		if (is_quote(str[i]))
+			return (1);
+	return (0);
+}
+
+int	send_variable(char *line, char *limitator, t_env_list *env)
+{
+	char	*flag;
+	char	*var;
+	char	*content;
+
+	var = NULL;
+	content = NULL;
+	flag = NULL;
+	while (line)
+	{
+		while (*line != '$')
+		{
+			flag = ft_strjoin_one_c(flag, *line);
+			(*line)++;
+		}
+		if (*line == '$')
+		{
+			var = check_variable(flag, ft_strlen(flag));
+			content = put_content_trim_of_expand(var, env);
+			flag = ft_strjoin(flag, content);
+			free(var);
+			free(content);
+		}
+	}
+}
+int	modify_heredoc(char **hdoc, char **limitator, t_env_list *env)
 {
 	// 1 splitter par les \n 
 	// checker chaque tab 
@@ -37,4 +75,3 @@ int	modify_heredoc(char *hdoc, char *limitator)
 	}
 	return (0);
 }
-
