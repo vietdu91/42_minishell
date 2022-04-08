c0c3f1ed31dba651db553bcb36927be19441c60f
Manu <tran.emmanuel@outlook.fr>
2022-04-08 20:04:01 +0200

    lol on se marre

--- srcs/exec/expand_heredoc.c
+++ srcs/exec/expand_heredoc.c
@@ -6,11 +6,23 @@
 /*   By: dyoula <dyoula@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/08 18:27:18 by dyoula            #+#    #+#             */
-/*   Updated: 2022/04/08 18:40:12 by dyoula           ###   ########.fr       */
+/*   Updated: 2022/04/08 19:01:28 by dyoula           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
+
+char	*what_value_(char *str, t_env_list *env)
+{
+	char	**cpy;
+
+	if (!str)
+		return ;
+	cpy = &str;
+	where_is_dollar(cpy, env);
+	convert_expand_quotes(parser);
+	return (cpy) ;
+}
 
 int	modify_heredoc(char *hdoc, char *limitator)
 {
