7175261ac137597b01d1a24cb176c7352f7175a0
Dimitri YOULA <dyoula@e2r8p13.clusters.42paris.fr>
2022-04-24 20:18:07 +0200

    exec working

---
+++ objs/main.d
@@ -0,0 +1,5 @@
+objs/main.o: srcs/main.c srcs/../includes/minishell.h \
+  includes/./parsing.h includes/./exec.h includes/./memory.h \
+  includes/./bultin.h includes/./checkers.h includes/./fun.h \
+  includes/./sig.h includes/./utils.h includes/./errors.h \
+  includes/./lexer.h
