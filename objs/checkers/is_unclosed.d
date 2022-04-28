7ad941cba4617bac183168ef42f9eeb60298ce24
QnYosa <dimitriyoula@gmail.com>
2022-04-28 23:12:00 +0200

    opt done

--- objs/checkers/is_unclosed.d
+++
@@ -1,5 +0,0 @@
-objs/checkers/is_unclosed.o: srcs/checkers/is_unclosed.c \
-  srcs/checkers/../../includes/minishell.h includes/./parsing.h \
-  includes/./exec.h includes/./memory.h includes/./bultin.h \
-  includes/./checkers.h includes/./fun.h includes/./sig.h \
-  includes/./utils.h includes/./errors.h includes/./lexer.h
--- objs/exec/path_manager.d
+++
@@ -1,5 +0,0 @@
-objs/exec/path_manager.o: srcs/exec/path_manager.c \
-  srcs/exec/../../includes/minishell.h includes/./parsing.h \
-  includes/./exec.h includes/./memory.h includes/./bultin.h \
-  includes/./checkers.h includes/./fun.h includes/./sig.h \
-  includes/./utils.h includes/./errors.h includes/./lexer.h
