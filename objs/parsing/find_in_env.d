6cdd4e5320571326cb42d6470c3daac3ed328a85
QnYosa <dimitriyoula@gmail.com>
2022-04-28 23:19:57 +0200

    opt done

--- objs/parsing/find_in_env.d
+++
@@ -1,5 +0,0 @@
-objs/parsing/find_in_env.o: srcs/parsing/find_in_env.c \
-  includes/../includes/minishell.h includes/./parsing.h \
-  includes/./exec.h includes/./memory.h includes/./bultin.h \
-  includes/./checkers.h includes/./fun.h includes/./sig.h \
-  includes/./utils.h includes/./errors.h includes/./lexer.h
