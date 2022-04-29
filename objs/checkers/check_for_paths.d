1d01edd66e983d782b33d4d5fd5167b6164cac71
Dimitri YOULA <dyoula@e2r1p10.clusters.42paris.fr>
2022-04-29 22:12:25 +0200

    multi args done

--- objs/checkers/check_for_paths.d
+++
@@ -1,5 +0,0 @@
-objs/checkers/check_for_paths.o: srcs/checkers/check_for_paths.c \
-  srcs/checkers/../../includes/minishell.h includes/./parsing.h \
-  includes/./exec.h includes/./memory.h includes/./bultin.h \
-  includes/./checkers.h includes/./fun.h includes/./sig.h \
-  includes/./utils.h includes/./errors.h includes/./lexer.h
--- objs/parsing/expand_variable.d
+++
@@ -1,5 +0,0 @@
-objs/parsing/expand_variable.o: srcs/parsing/expand_variable.c \
-  srcs/parsing/../../includes/minishell.h includes/./parsing.h \
-  includes/./exec.h includes/./memory.h includes/./bultin.h \
-  includes/./checkers.h includes/./fun.h includes/./sig.h \
-  includes/./utils.h includes/./errors.h includes/./lexer.h
