8ccdc136c24f171d7060f66d1c730979929c4392
Dimitri YOULA <dyoula@e2r1p10.clusters.42paris.fr>
2022-04-29 21:16:12 +0200

    bye

--- objs/bultin/env_change.d
+++
@@ -1,5 +0,0 @@
-objs/bultin/env_change.o: srcs/bultin/env_change.c \
-  includes/../includes/minishell.h includes/./parsing.h \
-  includes/./exec.h includes/./memory.h includes/./bultin.h \
-  includes/./checkers.h includes/./fun.h includes/./sig.h \
-  includes/./utils.h includes/./errors.h includes/./lexer.h
--- objs/utils/utils_str.d
+++
@@ -1,5 +0,0 @@
-objs/utils/utils_str.o: srcs/utils/utils_str.c \
-  srcs/utils/../../includes/minishell.h includes/./parsing.h \
-  includes/./exec.h includes/./memory.h includes/./bultin.h \
-  includes/./checkers.h includes/./fun.h includes/./sig.h \
-  includes/./utils.h includes/./errors.h includes/./lexer.h
