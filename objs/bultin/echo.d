2b48185828370810626c1321c1209bdd20d1e695
Emmanuel TRAN <emtran@e2r6p12.clusters.42paris.fr>
2022-05-03 15:32:37 +0200

    cd parfait ?

--- objs/bultin/echo.d
+++
@@ -1,5 +0,0 @@
-objs/bultin/echo.o: srcs/bultin/echo.c \
-  srcs/bultin/../../includes/minishell.h includes/./parsing.h \
-  includes/./exec.h includes/./memory.h includes/./bultin.h \
-  includes/./checkers.h includes/./fun.h includes/./sig.h \
-  includes/./utils.h includes/./errors.h includes/./lexer.h
